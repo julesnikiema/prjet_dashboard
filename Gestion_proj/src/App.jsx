12f64d65ba68a3adbc996d6bf6e949b37c17e1f5
NIKIEMA_ALASSANE_JULES <julesnikiema@gmail.com>
2023-08-09 10:45:12 +0100

    NEW STEP

--- Gestion_proj/src/App.jsx
+++ Gestion_proj/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import SingIn from './pages/auth/SignIn'
+import SingUp from './pages/auth/SingUp'
+import AuthDetails from './pages/AuthdDetails'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Home from './Home'
